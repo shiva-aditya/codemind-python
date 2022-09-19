0702c8ede71fff47e0d07d72c891c9301f385560
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-09-19 23:55:11 +0530

    def po(n):     for i in range(n[0],0,-1):         c=0         for j in n:             if j%i==0:                 c+=1         if c==len(n):             return i g=int(input()) t=list(map(int,input().split())) print(po(t))

---
+++ Minimum_Scale_length.py
@@ -0,0 +1,11 @@
+def po(n):
+    for i in range(n[0],0,-1):
+        c=0
+        for j in n:
+            if j%i==0:
+                c+=1
+        if c==len(n):
+            return i
+g=int(input())
+t=list(map(int,input().split()))
+print(po(t))

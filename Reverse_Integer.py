b28a8b16d804016d70f82d01c172e17e65132d33
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-09-21 22:49:05 +0530

    def po(n):     c=[]     if n<0:         c.append("-")         n=(-2*n)+n         while n:            s=n%10            n=n//10            c.append(s)         return mo(c)
        else:
            while n:
                s=n%10
                n=n//10
                c.append(s)
            return mo(c)
    def mo(a):
        if "-" in a:
            d=a[::-1]
            for i in range(len(d)-2,-1,-1):
                if d[i]==0:
                    d.remove(d[i])
                else:
                    break
            return d[::-1]
        else:
            d=a[::-1]
            for i in range(len(d)-1,-1,-1):
                if d[i]==0:
                    d.remove(d[i])
                else:
                    break
            return d[::-1]
            
           
    d=int(input())
    p=po(d)
    for v in p:
        print(v,end="")

---
+++ Reverse_Integer.py
@@ -0,0 +1,40 @@
+def po(n):
+    c=[]
+    if n<0:
+        c.append("-")
+        n=(-2*n)+n
+        while n:
+           s=n%10
+           n=n//10
+           c.append(s)
+        return mo(c)
+              
+    else:
+        while n:
+            s=n%10
+            n=n//10
+            c.append(s)
+        return mo(c)
+def mo(a):
+    if "-" in a:
+        d=a[::-1]
+        for i in range(len(d)-2,-1,-1):
+            if d[i]==0:
+                d.remove(d[i])
+            else:
+                break
+        return d[::-1]
+    else:
+        d=a[::-1]
+        for i in range(len(d)-1,-1,-1):
+            if d[i]==0:
+                d.remove(d[i])
+            else:
+                break
+        return d[::-1]
+        
+       
+d=int(input())
+p=po(d)
+for v in p:
+    print(v,end="")

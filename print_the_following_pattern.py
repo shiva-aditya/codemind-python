193fceee7219e9182918fc2a0466a10545ba9618
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-09-23 00:34:24 +0530

    a=int(input()) if a==0:     print(0) f="" for i in range(1,a+1):     f=f+str(i) for j in range(1,a+1):     print(f[0:j:])

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,10 @@
 a=int(input())
+if a==0:
+    print(0)
 f=""
-for i in range(a,0,-1):
-    f=f+str(i)+" "
-for j in range(a):
-    print(f)
+for i in range(1,a+1):
+    f=f+str(i)
+for j in range(1,a+1):
+    print(f[0:j:])
+    
+    

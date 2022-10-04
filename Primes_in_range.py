415b403e16e180bd68c55663b3a871d27163254c
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-04 19:23:46 +0530

    import math def po(n,m):    c=0    for i in range(n,m+1):       if p(i):          c+=1    return c
    def p(n):
       if n<=1:
          return False
       t=int(math.sqrt(n))
       for i in range(2,t+1):
          if n%i==0:
             return False
       return True
    
    
    a=int(input())
    b=int(input())
    print(po(a,b))

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,18 +1,21 @@
 import math
-def po(a,b):
-    v=[]
-    for i in range(a,b+1):
-        if i<1 or i==1:
-            continue
-        elif prime(i):
-            v.append(i)
-    return len(v)
-def prime(b):
-    d=int(math.sqrt(b))
-    for j in range(2,d+1):
-        if b%j==0:
-            return False
-    return True
-i=int(input())
-h=int(input())
-print(po(i,h))
+def po(n,m):
+   c=0
+   for i in range(n,m+1):
+      if p(i):
+         c+=1
+   return c
+
+def p(n):
+   if n<=1:
+      return False
+   t=int(math.sqrt(n))
+   for i in range(2,t+1):
+      if n%i==0:
+         return False
+   return True
+
+
+a=int(input())
+b=int(input())
+print(po(a,b))

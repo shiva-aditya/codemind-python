264db04c8479092e8711762e6e10b36dcbbf7d64
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-11 21:35:55 +0530

    def po(a,l):     c=0     for i in range(l-2):         if a[i]%2!=0 and a[i+2]%2==0:             c+=1         elif a[i]%2==0 and a[i+2]%2!=0:             c+=1     return c
    s=int(input())
    d=list(map(int,input().split()))
    print(po(d,s))

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.py
@@ -0,0 +1,15 @@
+def po(a,l):
+    c=0
+    for i in range(l-2):
+        if a[i]%2!=0 and a[i+2]%2==0:
+            c+=1
+        elif a[i]%2==0 and a[i+2]%2!=0:
+            c+=1
+    return c
+    
+    
+    
+    
+s=int(input())
+d=list(map(int,input().split()))
+print(po(d,s))

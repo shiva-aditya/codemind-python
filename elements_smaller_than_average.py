aab190cccc9c94955237dc08bb3cfd48323d4ef6
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-24 00:06:29 +0530

    def po(a,l):     s=sum(a)     d=s//l     c=0     for i in a:         if i<=d:             c+=1     return c
    s=int(input())
    p=list(map(int,input().split()))
    print(po(p,s))

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,15 @@
+def po(a,l):
+    s=sum(a)
+    d=s//l
+    c=0
+    for i in a:
+        if i<=d:
+            c+=1
+    return c
+
+
+
+
+s=int(input())
+p=list(map(int,input().split()))
+print(po(p,s))

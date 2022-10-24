2d4e17da6754713c42ff23829a0b09e034568121
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-24 20:52:08 +0530

    def po(a,n,m):     c=[]     for i in range(n,m+1):         c.append(i)     ans=[]     for i in a:         if i in c:             ans.append(i)     if len(ans)==0:         return [-1]     else:         return ans
    s=int(input())
    q=list(map(int,input().split()))
    a,b=map(int,input().split())
    print(*po(q,a,b))

---
+++ elements_in_between_A_and_B.py
@@ -0,0 +1,17 @@
+def po(a,n,m):
+    c=[]
+    for i in range(n,m+1):
+        c.append(i)
+    ans=[]
+    for i in a:
+        if i in c:
+            ans.append(i)
+    if len(ans)==0:
+        return [-1]
+    else:
+        return ans
+
+s=int(input())
+q=list(map(int,input().split()))
+a,b=map(int,input().split())
+print(*po(q,a,b))

dd6a1ba802452c4f64e9b6632cdd69ff7f723995
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-24 18:06:43 +0530

    def po(a):     e=[]     o=[]     for i in range(len(a)):         if a[i]%2==0:             e.append(a[i])         else:             o.append(a[i])     c=[]     if len(e)<len(o):         for i in range(len(e)):             c.append(o[i])             c.append(e[i])         q=len(o)-len(e)         for i in range(len(o)-q,len(o)):             c.append(o[i])         if q%2!=0:             c.append(0)     elif len(e)>len(o):         for i in range(len(o)):             c.append(o[i])             c.append(e[i])         q=len(e)-len(o)         for i in range(len(e)-q,len(e)):             c.append(e[i])         if q%2!=0:             c.append(0)     else:         for i in range(len(o)):             c.append(o[i])             c.append(e[i])     return c
    s=int(input())
    q=list(map(int,input().split()))
    print(*po(q))

---
+++ odd_even_pairs.py
@@ -0,0 +1,36 @@
+def po(a):
+    e=[]
+    o=[]
+    for i in range(len(a)):
+        if a[i]%2==0:
+            e.append(a[i])
+        else:
+            o.append(a[i])
+    c=[]
+    if len(e)<len(o):
+        for i in range(len(e)):
+            c.append(o[i])
+            c.append(e[i])
+        q=len(o)-len(e)
+        for i in range(len(o)-q,len(o)):
+            c.append(o[i])
+        if q%2!=0:
+            c.append(0)
+    elif len(e)>len(o):
+        for i in range(len(o)):
+            c.append(o[i])
+            c.append(e[i])
+        q=len(e)-len(o)
+        for i in range(len(e)-q,len(e)):
+            c.append(e[i])
+        if q%2!=0:
+            c.append(0)
+    else:
+        for i in range(len(o)):
+            c.append(o[i])
+            c.append(e[i])
+    return c
+
+s=int(input())
+q=list(map(int,input().split()))
+print(*po(q))

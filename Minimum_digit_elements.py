1e2056140f531fc682af6f8a7dce401d70805225
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-26 20:29:30 +0530

    def po(a,l):     d=[]     for i in a:         d.append(len(i))     dic={}     for i in d:         if i not in dic:             dic[i]=1         else:             dic[i]+=1#{2: 2, 3: 1, 1: 1, 4: 1}     e=min(dic)     return dic[e]
    s=int(input())
    q=list(map(str,input().split()))
    print(po(q,s))

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,16 @@
+def po(a,l):
+    d=[]
+    for i in a:
+        d.append(len(i))
+    dic={}
+    for i in d:
+        if i not in dic:
+            dic[i]=1
+        else:
+            dic[i]+=1#{2: 2, 3: 1, 1: 1, 4: 1}
+    e=min(dic)
+    return dic[e]
+    
+s=int(input())
+q=list(map(str,input().split()))
+print(po(q,s))

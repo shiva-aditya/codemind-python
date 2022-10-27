f3f58c11ad66be7861ad66ce95534d8f3da8f1e6
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-27 21:24:31 +0530

    def po(a):     ans=[]     y="aeiou"     for i in a:         c=0         for j in i:             if j in y:                 c+=1         ans.append(c)     return ans.count(max(ans))
    w=input()
    w=w.lower()
    w=w.split()
    print(po(w))

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,18 @@
+def po(a):
+    ans=[]
+    y="aeiou"
+    for i in a:
+        c=0
+        for j in i:
+            if j in y:
+                c+=1
+        ans.append(c)
+    return ans.count(max(ans))
+
+
+
+
+w=input()
+w=w.lower()
+w=w.split()
+print(po(w))
